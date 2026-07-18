e37481fc32205978e4bb00d4990b7a46b803b715
Willem van der Schyff <willemvds@gmail.com>
2026-07-18 13:16:03 +0200

    fear: Reap zombie processes.

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() -> io::Result<()> {
 
     eprintln!("playniceplease: pid={my_pid} uid={my_uid} gid={my_gid}; waiting for SIGTERM/SIGINT");
 
-    let sfd = wait_for_signal()?;
+    let sfd = run_signal_loop()?;
 
     let proc_fd = fs::openat(
         fs::CWD,
@@ -52,12 +52,13 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn wait_for_signal() -> io::Result<fd::OwnedFd> {
+fn run_signal_loop() -> io::Result<fd::OwnedFd> {
     let mut mask: libc::sigset_t = unsafe { mem::zeroed() };
     unsafe {
         libc::sigemptyset(&mut mask);
         libc::sigaddset(&mut mask, libc::SIGTERM);
         libc::sigaddset(&mut mask, libc::SIGINT);
+        libc::sigaddset(&mut mask, libc::SIGCHLD);
         if libc::sigprocmask(libc::SIG_BLOCK, &mask, std::ptr::null_mut()) != 0 {
             return Err(io::Error::last_os_error());
         }
@@ -75,12 +76,65 @@ fn wait_for_signal() -> io::Result<fd::OwnedFd> {
         if n == mem::size_of::<libc::signalfd_siginfo>() {
             let info: &libc::signalfd_siginfo =
                 unsafe { &*(ssi.as_ptr() as *const libc::signalfd_siginfo) };
-            let name = signal_name(info.ssi_signo as i32);
-            eprintln!("playniceplease: {name} received");
-            break;
+            match info.ssi_signo as i32 {
+                libc::SIGCHLD => reap_zombies(),
+                libc::SIGTERM | libc::SIGINT => {
+                    let name = signal_name(info.ssi_signo as i32);
+                    eprintln!("playniceplease: {name} received");
+                    break;
+                }
+                _ => {}
+            }
         }
     }
     Ok(sfd)
+}
+
+fn reap_zombies() {
+    loop {
+        match rx_process::waitid(
+            rx_process::WaitId::All,
+            rx_process::WaitIdOptions::EXITED | rx_process::WaitIdOptions::NOHANG,
+        ) {
+            Ok(Some(status)) => log_reaped(&status),
+            Ok(None) => break,
+            Err(rx_io::Errno::CHILD) => break,
+            Err(err) => {
+                eprintln!("playniceplease: waitid failed ({err})");
+                break;
+            }
+        }
+    }
+}
+
+fn log_reaped(status: &rx_process::WaitIdStatus) {
+    if status.exited() {
+        if let Some(code) = status.exit_status() {
+            eprintln!("playniceplease: reaped a zombie (exit code {code})");
+        } else {
+            eprintln!("playniceplease: reaped a zombie (exit code unavailable)");
+        }
+    } else if status.killed() {
+        if let Some(sig) = status.terminating_signal() {
+            eprintln!(
+                "playniceplease: reaped a zombie (killed by signal {sig} {})",
+                signal_name(sig)
+            );
+        } else {
+            eprintln!("playniceplease: reaped a zombie (killed by signal)");
+        }
+    } else if status.dumped() {
+        if let Some(sig) = status.terminating_signal() {
+            eprintln!(
+                "playniceplease: reaped a zombie (killed by signal {sig} {}, core dumped)",
+                signal_name(sig)
+            );
+        } else {
+            eprintln!("playniceplease: reaped a zombie (killed by signal, core dumped)");
+        }
+    } else {
+        eprintln!("playniceplease: reaped a zombie (state changed)");
+    }
 }
 
 fn scan_proc(proc_fd: fd::BorrowedFd<'_>) -> io::Result<Vec<PidEntry>> {
